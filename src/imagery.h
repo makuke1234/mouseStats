7379c0d130942ffebf2cf05629c0c2c0ae9b11e6
makuke1234 <makuke1234@gmail.com>
2022-10-25 23:52:01 +0300

    More organized imagery

--- src/imagery.h
+++ src/imagery.h
@@ -1,17 +1,7 @@
 #ifndef IMAGERY_H
 #define IMAGERY_H
 
-#include "winapi.h"
-
-typedef struct im_bmpHeader
-{
-	
-} im_bmpHeader_t;
-
-
-typedef struct im_bmpDib
-{
-	
-} im_bmpDib_t;
+#include "images/bmp.h"
+#include "images/png.h"
 
 #endif
---
+++ src/images/bmp.h
@@ -0,0 +1,17 @@
+#ifndef BMP_H
+#define BMP_H
+
+#include "../winapi.h"
+
+typedef struct im_bmpHeader
+{
+	
+} im_bmpHeader_t;
+
+
+typedef struct im_bmpDib
+{
+	
+} im_bmpDib_t;
+
+#endif
