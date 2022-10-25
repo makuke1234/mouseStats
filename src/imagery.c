c1d50373b0ae4ff972f5639baed0c2a46aac8c0c
makuke1234 <makuke1234@gmail.com>
2022-10-25 09:59:31 +0300

    Beginning of image handling

---
+++ src/imagery.c
@@ -0,0 +1,1 @@
+#include "imagery.h"
---
+++ src/imagery.h
@@ -0,0 +1,17 @@
+#ifndef IMAGERY_H
+#define IMAGERY_H
+
+#include "winapi.h"
+
+typedef struct im_bmpHeader
+{
+	
+} im_bmpHeader_t;
+
+
+typedef struct im_bmpDib
+{
+	
+} im_bmpDib_t;
+
+#endif
